89bef7596dad247af6a349ba1b3d9754ab385b7a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:47:16 -0600

    Rewriting as Laminas Project package

--- src/Wsdl/ComplexTypeStrategy/ComplexTypeStrategyInterface.php
+++ src/Wsdl/ComplexTypeStrategy/ComplexTypeStrategyInterface.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Soap
+ * @see       https://github.com/laminas/laminas-soap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-soap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-soap/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Soap\Wsdl\ComplexTypeStrategy;
+namespace Laminas\Soap\Wsdl\ComplexTypeStrategy;
 
-use Zend\Soap\Wsdl;
+use Laminas\Soap\Wsdl;
 
 /**
  * Interface strategies that generate an XSD-Schema for complex data types in WSDL files.
  *
- * @category   Zend
- * @package    Zend_Soap
+ * @category   Laminas
+ * @package    Laminas_Soap
  * @subpackage WSDL
  */
 interface ComplexTypeStrategyInterface
