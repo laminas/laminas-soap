dc361bc7a32b3881d6c97b35c39322a2942a7822
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:48:31 -0600

    Rewriting as Laminas Project package

--- src/Wsdl.php
+++ src/Wsdl.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-soap for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-soap/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-soap/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Soap;
+namespace Laminas\Soap;
 
-use DOMNode;
 use DOMDocument;
 use DOMDocumentFragment;
 use DOMElement;
+use DOMNode;
 use DOMXPath;
-use Zend\Soap\Wsdl\ComplexTypeStrategy\ComplexTypeStrategyInterface as ComplexTypeStrategy;
-use Zend\Uri\Uri;
+use Laminas\Soap\Wsdl\ComplexTypeStrategy\ComplexTypeStrategyInterface as ComplexTypeStrategy;
+use Laminas\Uri\Uri;
 
 class Wsdl
 {
