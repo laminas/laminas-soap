11aa7f314152d0c10c39b2039cab769477a29fd4
sandesh.as <cod48668@adobe.com>
2026-03-09 16:10:15 +0530

    Allow PHP 8.5 Support
    Signed-off-by: sandesh.as <cod48668@adobe.com>

--- src/Client/Common.php
+++ src/Client/Common.php
@@ -41,27 +41,22 @@ class Common extends SoapClient
      * Overridden to implement different transport layers, perform additional
      * XML processing or other purpose.
      *
+     * @param  string $request
+     * @param  string $location
+     * @param  string $action
+     * @param  int    $version
+     * @param  int    $oneWay
      * @return mixed
      */
     #[ReturnTypeWillChange]
-    public function __doRequest(
-        string $request,
-        string $location,
-        string $action,
-        int $version,
-        bool $oneWay = false,
-        ?string $uriParserClass = null
-    ): ?string {
+    public function __doRequest($request, $location, $action, $version, $oneWay = null, $uriParserClass = null)
+    {
         // ltrim is a workaround for https://bugs.php.net/bug.php?id=63780
-        return ($this->doRequestCallback)(
-            $this,
-            ltrim($request),
-            $location,
-            $action,
-            $version,
-            $oneWay,
-            $uriParserClass
-        );
+        if ($oneWay === null) {
+            return ($this->doRequestCallback)($this, ltrim($request), $location, $action, $version);
+        }
+
+        return ($this->doRequestCallback)($this, ltrim($request), $location, $action, $version, $oneWay, $uriParserClass);
     }
 
     /**
